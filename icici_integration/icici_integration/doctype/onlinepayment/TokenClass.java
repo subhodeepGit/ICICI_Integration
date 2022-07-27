3464b7022c6559ce959e5e4f11d083fab50c1630
Rupalib-soul <rupali.bhatta@soulunileaders.com>
2022-07-27 18:24:10 +0530

    added paymentgateway

---
+++ icici_integration/icici_integration/doctype/onlinepayment/TokenClass.java
@@ -0,0 +1,35 @@
+import com.fiserv.fdc.FDConnectUtils;
+import com.fiserv.fdc.sale.model.FDConnectSaleRequest;
+import com.fiserv.fdc.sale.model.FDConnectSaleResponse;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+
+public class TokenClass {
+
+    public static String getToken(String merchantId, String key, String iv, String apiURL, String amount, String currencyCode, String merchantTxnId, String transactionType, String resultURL) throws IOException {
+        
+        FDConnectSaleRequest request = new FDConnectSaleRequest(merchantId,key,iv,apiURL,amount,currencyCode,merchantTxnId,transactionType,resultURL);
+        FDConnectSaleResponse resp = FDConnectUtils.saleTxn(request);
+
+        System.out.println("resp SessionTokenId :" + resp.getSessionTokenId());
+        System.out.println("respErrorCode :"+resp.getErrorCode());
+        System.out.println("ErrorMessage :"+resp.getErrorMessage());  
+        
+        String tokenId=resp.getSessionTokenId();
+        return tokenId;
+    }
+   
+    public static String getDecryptResponse(String merchantId,String encData,String fdcTxnId,String apiURL) throws IOException {
+        
+            FDConnectDecryptRequest fdConnectDecryptRequest = new FDConnectDecryptRequest(merchantId,encData,fdcTxnId,apiURL);
+            FDConnectDecryptResponse resp = FDConnectUtils.decryptMsg(fdConnectDecryptRequest);
+    
+            System.out.println(resp.getErrorCode());
+            System.out.println(resp.getErrorMessage());
+            System.out.println(new Gson().toJson(resp));
+    }
+}
+
+
+   
--- icici_integration/icici_integration/doctype/onlinepayment/onlinepayment.py
+++ icici_integration/icici_integration/doctype/onlinepayment/onlinepayment.py
@@ -1,8 +1,56 @@
 # Copyright (c) 2022, SOUL ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+from jpype import startJVM, shutdownJVM, java, addClassPath, JClass, JInt
+# if jpype.isJVMStarted() is False:
+#  startJVM(convertStrings=False)
+addClassPath("/opt/bench/frappe-bench/apps/icici_integration/icici_integration/icici_integration/doctype/onlinepayment/TokenClass.jar")
+addClassPath("/opt/bench/frappe-bench/apps/icici_integration/icici_integration/icici_integration/doctype/onlinepayment/CommerceConnect.jar")
+startJVM(convertStrings=False)
+import jpype.imports
+import webbrowser
+from urllib.request import urlopen
+import frappe
 from frappe.model.document import Document
+from frappe.utils import get_url
 
 class OnlinePayment(Document):
-	pass
+    def validate(self):
+        print("\n\n\n")
+        print("Going to calc.py file")
+        print("\n\n\n")
+        
+       
+        getDoc=frappe.get_doc("ICICI Settings")
+        merchantId = getDoc.merchantid
+        print("\n\n\n")
+        print(merchantId)        
+        key=getDoc.key      
+        iv=getDoc.iv
+        apiURL="https://test.fdconnect.com//FirstPayL2Services/getToken"     
+        amountValue=self.amount
+        print("\n\n\n")
+        print(amountValue)      
+        currencyCode="INR" 
+        merchantTxnId=self.name  
+        transactionType="sale"          
+        resultURL="http://erp.soul.com:8000/app/onlinepayment/"+self.name
+        
+        print(resultURL)
+        try:
+
+            tokenclass = JClass('TokenClass') 
+            res = tokenclass.getToken(java.lang.String("%s"% merchantId), java.lang.String("%s"% key),
+                                java.lang.String("%s"%iv),java.lang.String("%s"% apiURL),
+                                java.lang.String("%s"% amountValue),java.lang.String("%s"% currencyCode),java.lang.String("%s"% merchantTxnId),
+                                java.lang.String("%s"% transactionType),java.lang.String("%s"% resultURL))
+            print(res)
+            newURL= "https://test.fdconnect.com//Pay/?sessionToken=" + str(res) + "&configId=PageId2022021713158"; 
+            # windows.location.redirect()            
+            webbrowser.open_new_tab(newURL) 
+            # status=tokenclass.getDecryptResponse()
+            
+        except Exception as err:
+            print("Exception: {err}")
+
+           
