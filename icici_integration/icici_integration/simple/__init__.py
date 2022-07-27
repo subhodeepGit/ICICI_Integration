ff76255bcd2bbd389966f1c0d2c8f36353aa33f4
Rupalib-soul <rupali.bhatta@soulunileaders.com>
2022-07-27 11:39:23 +0530

    Added new files for icici integration

--- icici_integration/icici_integration/simple/__init__.py
+++
@@ -1,4 +0,0 @@
-from simple.client_context import ClientContext
-from simple.gateway import Gateway
-from simple.merchant_credentials import MerchantCredentials
-from simple.signature import Signature 
